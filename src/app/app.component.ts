a1a9a3a8b0dd86570cb304f619b22261ed75f5b8
Sven Olding <sven.olding@gmx.de>
2024-01-10 11:24:43 +0100

    add custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,14 @@ export class AppComponent implements OnInit {
   submitted = false;
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
+      username: new FormControl(null, [
+        Validators.required,
+        this.forbiddenNames.bind(this),
+      ]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       gender: new FormControl(null, Validators.required),
       hobbies: new FormArray([]),
@@ -32,5 +36,13 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+
+    return null;
   }
 }
